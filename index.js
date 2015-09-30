2f805525585106a71f7024d4f806fd74633f8c30
wayne-o <wayne@prjatk.com>
2015-09-30 08:43:26 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-qunit-assert-html'
+  name: 'ember-cli-qunit-assert-html',
+  included: function(app) {
+    this._super.included(app);
+    app.import('vendor/qunit-assert-html.min.js');
+  }
 };
